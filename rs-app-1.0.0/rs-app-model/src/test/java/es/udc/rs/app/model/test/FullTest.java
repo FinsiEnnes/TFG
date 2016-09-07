a41344a34680c2f4c4c962ecb84a51e36dbc4025
FinsiEnnes <finsi.tic@gmail.com>
2016-09-07 19:37:02 +0200

    model

--- rs-app-1.0.0/rs-app-model/src/test/java/es/udc/rs/app/model/test/FullTest.java
+++ rs-app-1.0.0/rs-app-model/src/test/java/es/udc/rs/app/model/test/FullTest.java
@@ -1,6 +1,8 @@
 package es.udc.rs.app.model.test;
 
 
+import static org.junit.Assert.assertEquals;
+
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -15,9 +17,12 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import es.udc.rs.app.exceptions.InputValidationException;
 import es.udc.rs.app.exceptions.InstanceNotFoundException;
+import es.udc.rs.app.model.domain.AssignmentMaterial;
+import es.udc.rs.app.model.domain.AssignmentProfile;
 import es.udc.rs.app.model.domain.HistoryPerson;
 import es.udc.rs.app.model.domain.HistoryProject;
 import es.udc.rs.app.model.domain.LevelProfCatg;
+import es.udc.rs.app.model.domain.Material;
 import es.udc.rs.app.model.domain.Person;
 import es.udc.rs.app.model.domain.Phase;
 import es.udc.rs.app.model.domain.Priority;
@@ -26,7 +31,9 @@ import es.udc.rs.app.model.domain.Project;
 import es.udc.rs.app.model.domain.Province;
 import es.udc.rs.app.model.domain.State;
 import es.udc.rs.app.model.domain.Task;
+import es.udc.rs.app.model.service.assignment.AssignmentService;
 import es.udc.rs.app.model.service.customer.CustomerService;
+import es.udc.rs.app.model.service.material.MaterialService;
 import es.udc.rs.app.model.service.person.PersonService;
 import es.udc.rs.app.model.service.project.ProjectService;
 
@@ -45,13 +52,19 @@ public class FullTest {
 
 	@Autowired
 	private CustomerService customerService;
+	
+	@Autowired
+	private AssignmentService assignmentService;
+	
+	@Autowired
+	private MaterialService materialService;
 
 	@Test
 	public void fullTest() throws InstanceNotFoundException, InputValidationException, ParseException {
 
 		log.info("");
 		log.info ("============== Starting Full Test ==============");
-		thisTest();	
+		//thisTest();	
 	}
 
 	private void thisTest() throws InstanceNotFoundException, InputValidationException, ParseException {
@@ -60,30 +73,33 @@ public class FullTest {
 		// Variables
 		// ================================================================================
 
+		Task thisTask;
+		Integer num;
+		
 		SimpleDateFormat fmt = new SimpleDateFormat("yyyy-MM-dd");
 		Date iniPlanProject = fmt.parse("2016-01-01");
 		
-		Date iniPlanHistoryProject = fmt.parse("2016-01-01");
-		Date endPlanHistoryProject = fmt.parse("2016-02-28");
-		Date iniEjecHistoryProject = fmt.parse("2016-03-01");
+		Date iniPlanHistoryProject = fmt.parse("2016-08-15");
+		Date endPlanHistoryProject = fmt.parse("2016-08-28");
+		Date iniEjecHistoryProject = fmt.parse("2016-09-01");
 		Date endEjecHistoryProject = fmt.parse("2016-10-05");
 		
-		Date iniPhase1 = fmt.parse("2016-02-01");
-		Date endPhase1 = fmt.parse("2016-05-15");
-		Date iniPhase2 = fmt.parse("2016-05-15");
-		Date endPhase2 = fmt.parse("2016-10-10");
+		Date iniPhase1 = fmt.parse("2016-09-01");
+		Date endPhase1 = fmt.parse("2016-09-05");
+		Date iniPhase2 = fmt.parse("2016-09-05");
+		Date endPhase2 = fmt.parse("2016-09-12");
 		
-		Date iniPlanTask1 = fmt.parse("2016-02-05");
-		Date iniPlanTask2 = fmt.parse("2016-04-01");
-		Date iniPlanTask3 = fmt.parse("2016-05-15");
-		Date iniPlanTask4 = fmt.parse("2016-07-15");
-		Date iniPlanTask5 = fmt.parse("2016-09-01");
+		Date iniPlanTask1 = fmt.parse("2016-09-01");
+		Date iniPlanTask2 = fmt.parse("2016-09-04");
+		Date iniPlanTask3 = fmt.parse("2016-09-05");
+		Date iniPlanTask4 = fmt.parse("2016-09-07");
+		Date iniPlanTask5 = fmt.parse("2016-09-09");
 
-		Integer daysPlanTask1 = 56;
-		Integer daysPlanTask2 = 44;
-		Integer daysPlanTask3 = 61;
-		Integer daysPlanTask4 = 48;
-		Integer daysPlanTask5 = 30;
+		Integer daysPlanTask1 = 3;
+		Integer daysPlanTask2 = 1;
+		Integer daysPlanTask3 = 2;
+		Integer daysPlanTask4 = 2;
+		Integer daysPlanTask5 = 3;
 		
 		// ================================================================================
 		log.info("");
@@ -122,6 +138,7 @@ public class FullTest {
 		HistoryPerson hpManager = new HistoryPerson(person1, manager, fmt.parse("2013-05-06"), null, 12, 14, null);
 		personService.createHistoryPerson(hpManager);
 		
+		
 		// ================================================================================
 		log.info("");
 		log.info("===> Current Project State: PLAN");
@@ -145,19 +162,19 @@ public class FullTest {
 		log.info("");
 		log.info("===> Correct create of Tasks");
 		// ================================================================================
-		Task task1 = new Task(phase1, "Task 1", plan, m, hpManager, daysPlanTask1);
+		Task task1 = new Task(phase1, "Tarea 1", plan, m, hpManager, daysPlanTask1);
 		task1.setIniPlan(iniPlanTask1);
 		
-		Task task2 = new Task(phase1, "Task 2", plan, m, hpManager, daysPlanTask2);
+		Task task2 = new Task(phase1, "Tarea 2", plan, m, hpManager, daysPlanTask2);
 		task2.setIniPlan(iniPlanTask2);
 		
-		Task task3 = new Task(phase2, "Task 3", plan, m, hpManager, daysPlanTask3);
+		Task task3 = new Task(phase2, "Tarea 3", plan, m, hpManager, daysPlanTask3);
 		task3.setIniPlan(iniPlanTask3);
 		
-		Task task4 = new Task(phase2, "Task 4", plan, m, hpManager, daysPlanTask4);
+		Task task4 = new Task(phase2, "Tarea 4", plan, m, hpManager, daysPlanTask4);
 		task4.setIniPlan(iniPlanTask4);
 		
-		Task task5 = new Task(phase2, "Task 5", plan, m, hpManager, daysPlanTask5);
+		Task task5 = new Task(phase2, "Tarea 5", plan, m, hpManager, daysPlanTask5);
 		task5.setIniPlan(iniPlanTask5);
 		
 		projectService.createTask(task1);
@@ -165,6 +182,128 @@ public class FullTest {
 		projectService.createTask(task3);
 		projectService.createTask(task4);
 		projectService.createTask(task5);
+		
+		
+		// ================================================================================
+		log.info("");
+		log.info("===> Creation of Professional Categories");
+		// ================================================================================	
+		ProfessionalCategory profCatg1 = new ProfessionalCategory("Perfil1", 1, senior, 6, null);
+		ProfessionalCategory profCatg2 = new ProfessionalCategory("Perfil2", 1, senior, 8, null);
+		ProfessionalCategory profCatg3 = new ProfessionalCategory("Perfil3", 1, senior, 11, null);
+		
+		personService.createProfessionalCategory(profCatg1);
+		personService.createProfessionalCategory(profCatg2);
+		personService.createProfessionalCategory(profCatg3);
+		
+		
+		// ================================================================================
+		log.info("");
+		log.info("===> Assignment of the Profiles in the Task");
+		// ================================================================================
+		AssignmentProfile ap1 = new AssignmentProfile(task1, profCatg1, 1, 20, 0);
+		AssignmentProfile ap2 = new AssignmentProfile(task1, profCatg2, 1, 10, 0);
+		AssignmentProfile ap3 = new AssignmentProfile(task1, profCatg3, 1, 8, 0);
+		
+		AssignmentProfile ap4 = new AssignmentProfile(task2, profCatg2, 1, 7, 0);
+		
+		AssignmentProfile ap5 = new AssignmentProfile(task3, profCatg1, 1, 16, 0);
+		AssignmentProfile ap6 = new AssignmentProfile(task3, profCatg2, 1, 8, 0);
+		
+		AssignmentProfile ap7 = new AssignmentProfile(task4, profCatg3, 1, 12, 0);
+		
+		AssignmentProfile ap8 = new AssignmentProfile(task5, profCatg1, 1, 15, 0);
+		AssignmentProfile ap9 = new AssignmentProfile(task5, profCatg2, 1, 12, 0);
+		AssignmentProfile ap10 = new AssignmentProfile(task5, profCatg3, 1, 9, 0);
+		
+		assignmentService.createAssignmentProfile(ap1);
+		assignmentService.createAssignmentProfile(ap2);
+		assignmentService.createAssignmentProfile(ap3);
+		assignmentService.createAssignmentProfile(ap4);
+		assignmentService.createAssignmentProfile(ap5);
+		assignmentService.createAssignmentProfile(ap6);
+		assignmentService.createAssignmentProfile(ap7);
+		assignmentService.createAssignmentProfile(ap8);
+		assignmentService.createAssignmentProfile(ap9);
+		assignmentService.createAssignmentProfile(ap10);
+		
+		
+		// ================================================================================
+		log.info("");
+		log.info("===> Creation of the Materials");
+		// ================================================================================	
+		Material material1 = new Material("Material1",null, 60, false);
+		Material material2 = new Material("Material2",null, 130, false);
+		
+		materialService.createMaterial(material1);
+		materialService.createMaterial(material2);
+		
+		
+		// ================================================================================
+		log.info("");
+		log.info("===> Assignment of the Materials in the Task");
+		// ================================================================================
+		AssignmentMaterial assigMat1 = new AssignmentMaterial(task1, material1);
+		assigMat1.setUnitsPlan(1);
+		
+		AssignmentMaterial assigMat2 = new AssignmentMaterial(task5, material2);
+		assigMat2.setUnitsPlan(1);
+		
+		assignmentService.createAssignmentMaterial(assigMat1);
+		assignmentService.createAssignmentMaterial(assigMat2);
+		
+		
+		// ================================================================================
+		log.info("");
+		log.info("===> Change Task State: PLAN -> PRPR");
+		// ================================================================================	
+		task1.setState(prpd);
+		projectService.updateTask(task1);
+		
+		task2.setState(prpd);
+		projectService.updateTask(task2);
+		
+		task3.setState(prpd);
+		projectService.updateTask(task3);
+		
+		task4.setState(prpd);
+		projectService.updateTask(task4);
+		
+		task5.setState(prpd);
+		projectService.updateTask(task5);
+		
+		
+		// Check the final costs
+		thisTask = projectService.findTask(task1.getId());
+		num = 348;
+		assertEquals(num, thisTask.getCostPlan());
+		
+		thisTask = projectService.findTask(task2.getId());
+		num = 56;
+		assertEquals(num, thisTask.getCostPlan());
+		
+		
+		thisTask = projectService.findTask(task3.getId());
+		num = 160;
+		assertEquals(num, thisTask.getCostPlan());
+		
+		thisTask = projectService.findTask(task4.getId());
+		num = 132;
+		assertEquals(num, thisTask.getCostPlan());
+		
+		thisTask = projectService.findTask(task5.getId());
+		num = 415;
+		assertEquals(num, thisTask.getCostPlan());
+		
+		
+		// ================================================================================
+		log.info("");
+		log.info("===> Change Project State: PLAN -> EJEC");
+		// ================================================================================
+		HistoryProject ejecHP = new HistoryProject(project, ejec, iniEjecHistoryProject, endEjecHistoryProject, null);
+		projectService.createHistoryProject(ejecHP);
+		
+		
 	}
 
 }
