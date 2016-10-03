d409c25dde2f5dc2a7b3cfcf73d4fa251634ef75
FinsiEnnes <finsi.tic@gmail.com>
2016-10-03 11:20:30 +0200

    Adding the client layer structure

---
+++ rs-app-1.0.0/rs-app-client/src/main/java/es/udc/rs/app/client/app/Application.java
@@ -0,0 +1,31 @@
+package es.udc.rs.app.client.app;
+
+import java.util.Arrays;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
+
+@Configuration
+@ComponentScan({ "es/udc/rs/app/client/app" })
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
+@ImportResource("file:/home/finsi/myProject/rs-app-1.0.0/rs-app-model/src/main/resources/spring-config.xml")
+public class Application {
+    
+	public static void main(String[] args) {
+        
+        ApplicationContext ctx = SpringApplication.run(Application.class, args);
+
+        System.out.println("Let's inspect the beans provided by Spring Boot:");
+
+        String[] beanNames = ctx.getBeanDefinitionNames();
+        Arrays.sort(beanNames);
+        for (String beanName : beanNames) {
+            System.out.println(beanName);
+        }
+    }
+}
---
+++ rs-app-1.0.0/rs-app-client/src/main/java/es/udc/rs/app/client/app/GlobalControllerExceptionHandler.java
@@ -0,0 +1,63 @@
+package es.udc.rs.app.client.app;
+
+import org.hibernate.exception.GenericJDBCException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.dao.DataAccessException;
+import org.springframework.http.HttpStatus; 
+
+import es.udc.rs.app.exceptions.InputValidationException;
+import es.udc.rs.app.exceptions.InstanceNotFoundException;
+
+@ControllerAdvice
+public class GlobalControllerExceptionHandler {
+
+	@ResponseStatus(HttpStatus.BAD_REQUEST)  // 400
+	@ExceptionHandler(InputValidationException.class)
+	public ModelAndView InputValidation(InputValidationException e) {
+
+		ModelAndView mav = new ModelAndView("exception");
+		mav.addObject("name", e.getClass().getSimpleName());
+		mav.addObject("message", e.getMessage());
+
+		return mav;
+	}
+
+
+	@ResponseStatus(HttpStatus.FORBIDDEN)  // 403
+	@ExceptionHandler(GenericJDBCException.class)
+	public ModelAndView GenericJDBC(GenericJDBCException e) {
+
+		ModelAndView mav = new ModelAndView("exception");
+		mav.addObject("name", e.getClass().getSimpleName());
+		mav.addObject("message", e.getCause().getCause().getMessage());
+
+		return mav;
+	}
+
+
+	@ResponseStatus(HttpStatus.NOT_FOUND)  // 404
+	@ExceptionHandler(InstanceNotFoundException.class)
+	public ModelAndView InstanceNotFound(InstanceNotFoundException e) {
+
+		ModelAndView mav = new ModelAndView("exception");
+		mav.addObject("name", e.getClass().getSimpleName());
+		mav.addObject("message", e.getMessage());
+
+		return mav;
+	}
+
+
+	@ResponseStatus(HttpStatus.CONFLICT)  // 409
+	@ExceptionHandler(DataAccessException.class)
+	public ModelAndView DataAccess(DataAccessException e) {
+
+		ModelAndView mav = new ModelAndView("exception");
+		mav.addObject("name", e.getClass().getSimpleName());
+		mav.addObject("message", e.getCause().getCause().getMessage());
+
+		return mav;
+	}
+}
